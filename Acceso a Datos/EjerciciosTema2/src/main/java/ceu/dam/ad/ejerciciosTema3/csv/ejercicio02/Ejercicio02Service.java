bd78b71ddc8f66060892a7f8932cd300a361d53e
fcabrera <fcabrera4084@ceuandalucia.es>
2024-11-07 17:42:48 +0100

    organizo mejor esto

--- Acceso a Datos/EjerciciosTema2/src/main/java/ceu/dam/ad/ejerciciosTema3/csv/ejercicio02/Ejercicio02Service.java
+++
@@ -1,9 +0,0 @@
-package ceu.dam.ad.ejerciciosTema3.csv.ejercicio02;
-
-import ceu.dam.ad.ejerciciosTema3.csv.exceptions.CsvException;
-
-public interface Ejercicio02Service {
-
-	public void importarUsuarioCSV(String nombreFichero) throws CsvException;
-
-}
--- Acceso a Datos/EjerciciosTema2/src/main/java/ceu/dam/ad/ejerciciosTema3/csv/ejercicio03/Ejercicio03Service.java
+++
@@ -1,9 +0,0 @@
-package ceu.dam.ad.ejerciciosTema3.csv.ejercicio03;
-
-import ceu.dam.ad.ejerciciosTema3.csv.exceptions.CsvException;
-
-public interface Ejercicio03Service {
-
-	void convertirCsv(String ficheroEntrada, String ficheroSalida) throws CsvException;
-
-}
